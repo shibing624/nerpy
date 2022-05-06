674ef5449152e148b7b02db758cba44eac12bc3c
shibing624 <shibing624@126.com>
2022-05-06 21:10:52 +0800

    update pytest.

--- tests/test_issue.py
+++ tests/test_issue.py
@@ -4,14 +4,13 @@
 @description: 
 """
 import sys
-import pytest
 
 sys.path.append('..')
 
 from nerpy.ner_model import NERModel
 
 
-def test_diff(self):
+def test_diff():
     a = '研究团队面向国家重大战略需求追踪国际前沿发展借鉴国际人工智能研究领域的科研模式有效整合创新资源解决复'
     b = '英汉互译比较语言学'
     model = NERModel(
