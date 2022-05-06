9e2edbca8e28f7fcc4dbdc610407a76d5d0c09cc
shibing624 <shibing624@126.com>
2022-05-06 21:09:41 +0800

    update pytest.

--- tests/test_issue.py
+++ tests/test_issue.py
@@ -4,33 +4,27 @@
 @description: 
 """
 import sys
-import unittest
+import pytest
 
 sys.path.append('..')
 
 from nerpy.ner_model import NERModel
 
 
-class IssueTestCase(unittest.TestCase):
-
-    def test_sim_diff(self):
-        a = '研究团队面向国家重大战略需求追踪国际前沿发展借鉴国际人工智能研究领域的科研模式有效整合创新资源解决复'
-        b = '英汉互译比较语言学'
-        model = NERModel(
-            "bert",
-            "bert-base-chinese",
-            args={"overwrite_output_dir": True,
-                  "reprocess_input_data": True,
-                  "output_dir": "./output/",
-                  "max_seq_length": 128,
-                  "num_train_epochs": 3,
-                  "train_batch_size": 32,
-                  },
-            use_cuda=False
-        )
-        print(model)
-        self.assertTrue(model is not None)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_diff(self):
+    a = '研究团队面向国家重大战略需求追踪国际前沿发展借鉴国际人工智能研究领域的科研模式有效整合创新资源解决复'
+    b = '英汉互译比较语言学'
+    model = NERModel(
+        "bert",
+        "bert-base-chinese",
+        args={"overwrite_output_dir": True,
+              "reprocess_input_data": True,
+              "output_dir": "./output/",
+              "max_seq_length": 128,
+              "num_train_epochs": 3,
+              "train_batch_size": 32,
+              },
+        use_cuda=False
+    )
+    print(model)
+    assert model is not None
