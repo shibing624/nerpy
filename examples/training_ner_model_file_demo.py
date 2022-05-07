9ac564aade684807da91439c54a12cb01cf2b2ea
shibing624 <shibing624@126.com>
2022-05-07 16:03:28 +0800

    update training demo.

--- examples/training_ner_model_file_demo.py
+++ examples/training_ner_model_file_demo.py
@@ -20,11 +20,12 @@ def main():
     parser.add_argument('--task_name', default='cner', const='cner', nargs='?',
                         choices=['cner', 'people'], help='task name of dataset')
     parser.add_argument('--model_type', default='bert', type=str, help='Transformers model type')
-    parser.add_argument('--model_name', default='hfl/chinese-macbert-base', type=str, help='Transformers model or path')
+    parser.add_argument('--model_name', default='bert-base-chinese', type=str, help='Transformers model or path')
     parser.add_argument('--do_train', action='store_true', help='Whether to run training.')
     parser.add_argument('--do_predict', action='store_true', help='Whether to run predict.')
     parser.add_argument('--use_cuda', action='store_true', help='Whether to use cuda.')
-    parser.add_argument('--output_dir', default='./outputs/cner-model', type=str, help='Model output directory')
+    parser.add_argument('--output_dir', default='./outputs/', type=str, help='Model output directory')
+    parser.add_argument('--best_model_dir', default='./outputs/best_model/', type=str, help='Model output directory')
     parser.add_argument('--max_seq_length', default=128, type=int, help='Max sequence length')
     parser.add_argument('--num_epochs', default=4, type=int, help='Number of training epochs')
     parser.add_argument('--batch_size', default=32, type=int, help='Batch size')
@@ -41,34 +42,40 @@ def main():
     logger.info(f'train labels: {train_labels}')
     logger.info(f'train shape: {train_data.shape}, eval shape: {eval_data.shape}, test shape: {test_data.shape}')
 
-    # Create a NERModel
-    model = NERModel(
-        args.model_type,
-        args.model_name,
-        labels=train_labels,
-        args={"overwrite_output_dir": True,
-              "reprocess_input_data": True,
-              "output_dir": args.output_dir,
-              "max_seq_length": args.max_seq_length,
-              "num_train_epochs": args.num_epochs,
-              "train_batch_size": args.batch_size,
-              "classification_report": True,
-              "evaluate_during_training": True,
-              },
-        use_cuda=args.use_cuda,
-    )
     if args.do_train:
+        # Create a NERModel
+        model = NERModel(
+            args.model_type,
+            args.model_name,
+            labels=train_labels,
+            args={"overwrite_output_dir": True,
+                  "reprocess_input_data": True,
+                  "output_dir": args.output_dir,
+                  "best_model_dir": args.best_model_dir,
+                  "max_seq_length": args.max_seq_length,
+                  "num_train_epochs": args.num_epochs,
+                  "train_batch_size": args.batch_size,
+                  "classification_report": True,
+                  "evaluate_during_training": True,
+                  },
+            use_cuda=args.use_cuda,
+        )
         # Train the model
         model.train_model(train_data, eval_data=eval_data)
 
     if args.do_predict:
+        model = NERModel(
+            args.model_type,
+            args.output_dir,
+            labels=train_labels,
+            use_cuda=args.use_cuda,
+        )
         # Evaluate the model with test data
         result, model_outputs, predictions = model.eval_model(test_data)
         print(result)
 
         # Predictions on text strings
         sentences = [
-            "前不久，报纸上刊登了一个新闻照片，是美国总统拜登到墨尔本访问了福特澳大利亚公司。",
             "常建良，男，1963年出生，工科学士，高级工程师，北京物资学院客座副教授",
             "1985年8月-1993年在国家物资局、物资部、国内贸易部金属材料流通司从事国家统配钢材中特种钢材品种的调拨分配工作，先后任科员、主任科员。"
         ]
